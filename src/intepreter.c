7a78a63c2df0332240e9dc83db5581ccfd3774f7
bomba-cat <bledion.morina@hotmail.com>
2024-08-22 10:51:55 -0500

    fixed the file path by buffering

--- src/intepreter.c
+++ src/intepreter.c
@@ -2,8 +2,12 @@
 
 //Returns status and error code of execution, anything over 0 is an error
 int executeFile(char *path[]){
+    //Buffering the path
+    char buf[0x100];
+    snprintf(buf, sizeof(buf), "%s.vbf", path);
+    //Ready up the executable
     FILE *executable;
-    executable = fopen(path, "rb");
+    executable = fopen(buf, "rb");
 
     return 0;
 }
