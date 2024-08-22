e6fd47dbddb89df8a674493172995d61df644cc8
bomba-cat <bledion.morina@hotmail.com>
2024-08-22 10:48:36 -0500

    should start reading the executable as binary now

--- src/intepreter.c
+++ src/intepreter.c
@@ -2,7 +2,8 @@
 
 //Returns status and error code of execution, anything over 0 is an error
 int executeFile(char *path[]){
-    
+    FILE *executable;
+    executable = fopen(path, "rb");
 
     return 0;
 }
